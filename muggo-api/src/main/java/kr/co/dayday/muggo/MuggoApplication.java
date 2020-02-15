5a99de63c8dd981f6c5dde2bd08e7d3efe84dfde
jongminshin <ace4463@gmail.com>
2020-02-15 20:12:21 +0900

    Initial commit

---
+++ muggo-api/src/main/java/kr/co/dayday/muggo/MuggoApplication.java
@@ -0,0 +1,13 @@
+package kr.co.dayday.muggo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuggoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuggoApplication.class, args);
+	}
+
+}
---
+++ muggo-api/src/test/java/kr/co/dayday/muggo/MuggoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.dayday.muggo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuggoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
