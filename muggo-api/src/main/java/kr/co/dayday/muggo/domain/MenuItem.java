39fe8c8dbc87c4619f234edc5bcc1a35f6b5ee0a
jongminshin <ace4463@gmail.com>
2020-02-25 18:17:09 +0900

    JPA 영속화 작업

--- muggo-api/src/main/java/kr/co/dayday/muggo/domain/MenuItem.java
+++ muggo-api/src/main/java/kr/co/dayday/muggo/domain/MenuItem.java
@@ -1,6 +1,17 @@
 package kr.co.dayday.muggo.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class MenuItem {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long restaurantId;
 
     private final String name;
 
