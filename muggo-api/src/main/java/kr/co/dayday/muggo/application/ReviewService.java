a0d47b89e0a4b4eb71366cd543e3113efd64e1a9
jongminshin <ace4463@gmail.com>
2020-04-02 22:27:59 +0900

    ReviewController 작성

---
+++ muggo-api/src/main/java/kr/co/dayday/muggo/application/ReviewService.java
@@ -0,0 +1,11 @@
+package kr.co.dayday.muggo.application;
+
+import kr.co.dayday.muggo.domain.Review;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    public void addReview(Review review) {
+        //TODO:
+    }
+}
